1762a48ade23ce77c1c4bbfda49b4e1dd8836b95
LJR411~ <ljremulla@yahoo.com>
2023-06-20 12:52:40 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function incLikeNiel(){
+    var num = document.querySelector("#niellike")
+    num.innerText = (Number(num.innerText) + 1)
+
+}
+
+function incLikeNich(){
+    var num = document.querySelector("#nichlike")
+    num.innerText = (Number(num.innerText) + 1)
+
+}
+
+function incLikeJim(){
+    var num = document.querySelector("#jimlike")
+    num.innerText = (Number(num.innerText) + 1)
+
+}
